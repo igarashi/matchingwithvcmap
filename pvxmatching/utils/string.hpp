b1bc5441d7b45622edf58f0597f478c07ee6db75
Yuki IGARASHI <me@bonprosoft.com>
2017-10-17 13:53:11 +0900

    Update utils.

--- pvxmatching/utils/string.hpp
+++ pvxmatching/utils/string.hpp
@@ -9,6 +9,12 @@
 #include <string>
 #include <stdexcept>
 
+#ifdef __MINGW32__
+#define vxx_printf _snprintf
+#else
+#define vxx_printf std::snprintf
+#endif
+
 namespace utils {
 namespace string {
 
@@ -31,13 +37,13 @@ std::string strprintf(const std::string &format, const Args &... args) {
   std::string str;
   str.resize(1024);
 
-  auto size = _snprintf(&str[0], str.size(), format.c_str(), detail::to_printable(args)...);
+  auto size = vxx_printf(&str[0], str.size(), format.c_str(), detail::to_printable(args)...);
   if (size < 0)
     throw std::invalid_argument("Invalid format");
 
   if (size > str.size()) {
     str.resize(size + 1);
-    size = _snprintf(&str[0], str.size(), format.c_str(), detail::to_printable(args)...);
+    size = vxx_printf(&str[0], str.size(), format.c_str(), detail::to_printable(args)...);
   }
 
   str.resize(size);
